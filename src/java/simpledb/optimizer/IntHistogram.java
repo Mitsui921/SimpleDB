c4ca9651cc2b6f44824feccbbb32e828f8c96ba4
ChiangT <smudge0xc00000f@gmail.com>
2023-07-14 18:02:07 +0800

    complete IntHistogram

--- src/java/simpledb/optimizer/IntHistogram.java
+++ src/java/simpledb/optimizer/IntHistogram.java
@@ -2,10 +2,50 @@ package simpledb.optimizer;
 
 import simpledb.execution.Predicate;
 
+import java.util.Arrays;
+
 /**
  * A class to represent a fixed-width histogram over a single integer-based field.
  */
 public class IntHistogram {
+
+    private int buckets;
+    private int min;
+    private int max;
+    private double avg;
+    private Gram[] grams;
+    private int ntups;
+
+    public class Gram{
+        double left;
+        double right;
+        double w;
+        int h;
+
+        public Gram(double left, double right){
+            this.left = left;
+            this.right = right;
+            this.w = this.right - this.left;
+            this.h = 0;
+        }
+
+        public boolean inRange(int v){
+            if(v >= left && v <= right){
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public String toString(){
+            return "Gram{" +
+                    "left=" + left +
+                    ", right=" + right +
+                    ", w=" + w +
+                    ", h=" + h +
+                    "}";
+        }
+    }
 
     /**
      * Create a new IntHistogram.
@@ -24,7 +64,37 @@ public class IntHistogram {
      * @param max     The maximum integer value that will ever be passed to this class for histogramming
      */
     public IntHistogram(int buckets, int min, int max) {
-        // TODO: some code goes here
+        // some code goes here
+        this.buckets = buckets;
+        this.min = min;
+        this.max = max;
+        this.ntups = 0;
+        this.grams = new Gram[buckets];
+        this.avg = (double) (max - min) / (double) buckets;
+        double l = min;
+        if (this.avg % 1 != 0){
+            this.avg = (int) (this.avg + 1);
+        }
+        for (int i=0; i<buckets; i++){
+            grams[i] = new Gram(l, l+avg);
+            l += avg;
+        }
+    }
+
+    public int binarySearch(int v){
+        int l = 0;
+        int r = buckets - 1;
+        while (l <= r){
+            int mid = (l+r)/2;
+            if(grams[mid].inRange(v)){
+                return mid;
+            } else if(grams[mid].left > v){
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return -1;
     }
 
     /**
@@ -33,7 +103,16 @@ public class IntHistogram {
      * @param v Value to add to the histogram
      */
     public void addValue(int v) {
-        // TODO: some code goes here
+        // some code goes here
+        int i = binarySearch(v);
+        if (i != -1){
+            grams[i].h++;
+            ntups++;
+        }
+    }
+
+    public void setNtups(int ntups){
+        this.ntups = ntups;
     }
 
     /**
@@ -47,9 +126,84 @@ public class IntHistogram {
      * @return Predicted selectivity of this particular operator and value
      */
     public double estimateSelectivity(Predicate.Op op, int v) {
-
-        // TODO: some code goes here
-        return -1.0;
+        // some code goes here
+        int i = binarySearch(v);
+        Gram gram;
+        gram = i != -1 ? grams[i] : null;
+        if (op == Predicate.Op.EQUALS){
+            if (gram != null){
+                return (gram.h / gram.w) / (double) ntups;
+            } else {
+                return 0.0;
+            }
+        } else if (op == Predicate.Op.GREATER_THAN){
+            if (v < min){
+                return 1.0;
+            } else if (v >= max){
+                return 0.0;
+            } else if (gram != null){
+                double res = 0;
+                double add = ((double) gram.h / (double) ntups) * ((gram.right - v) / gram.w);
+                res += add;
+                int j;
+                for (j = i+1; j<buckets; j++){
+                    res += (double) grams[j].h / (double) ntups;
+                }
+                return res;
+            }
+        } else if (op == Predicate.Op.LESS_THAN){
+            if (v <= min){
+                return 0.0;
+            } else if (v >= max){
+                return 1.0;
+            } else if (gram != null){
+                double res = 0;
+                double add = ((double) gram.h / (double) ntups * (v - gram.left) / gram.w);
+                res += add;
+                int j;
+                for (j = 0; j<i; j++){
+                    res += (double) grams[j].h / (double) ntups;
+                }
+                return res;
+            }
+        } else if (op == Predicate.Op.NOT_EQUALS){
+            if (gram != null){
+                return 1 - (gram.h / gram.w / (double) ntups);
+            } else {
+                return 1.0;
+            }
+        } else if (op == Predicate.Op.GREATER_THAN_OR_EQ){
+            if (v <= min){
+                return 1.0;
+            } else if (v > max){
+                return 0.0;
+            } else if (gram != null){
+                double res = 0;
+                double add = ((double) gram.h * ((gram.right - v + 1) / gram.w)) / (double) ntups;
+                res += add;
+                int j;
+                for (j = i+1; j<buckets; j++){
+                    res += (double) grams[j].h / (double) ntups;
+                }
+                return res;
+            }
+        } else if (op == Predicate.Op.LESS_THAN_OR_EQ){
+            if (v < min){
+                return 0.0;
+            } else if (v >= max){
+                return 1.0;
+            } else if (gram != null){
+                double res = 0;
+                double add = ((double) gram.h / (double) ntups) * ((v - gram.left + 1) / gram.w);
+                res += add;
+                int j;
+                for (j=0; j<i; j++){
+                    res += (double) grams[j].h / (double) ntups;
+                }
+                return res;
+            }
+        }
+        return 0.0;
     }
 
     /**
@@ -60,15 +214,22 @@ public class IntHistogram {
      *         implement a more efficient optimization
      */
     public double avgSelectivity() {
-        // TODO: some code goes here
-        return 1.0;
+        // some code goes here
+        return avg;
     }
 
     /**
      * @return A string describing this histogram, for debugging purposes
      */
+    @Override
     public String toString() {
-        // TODO: some code goes here
-        return null;
+        // some code goes here
+        return "IntHistogram{" +
+                "buckets=" + buckets +
+                ", min=" + min +
+                ", max=" + max +
+                ", avg=" + avg +
+                ", grams=" + Arrays.toString(grams) +
+                "}";
     }
 }
